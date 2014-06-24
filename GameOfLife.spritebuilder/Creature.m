cbb2376e2862cefaf8b409eefb689745f0e83ea2
Andrew Brandt <Andrew@Xerxes.local>
2014-06-24 11:16:03 -0400

    Automatically Committed for MakeGamesWithUs

---
+++ GameOfLife.spritebuilder/Creature.m
@@ -0,0 +1,32 @@
+//
+//  Creature.m
+//  GameOfLife
+//
+//  Created by Andrew Brandt on 6/24/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Creature.h"
+
+@implementation Creature
+
+- (id)initCreature {
+
+    self = [super initWithImageNamed:@"GameOfLifeAssets/Assets/bubble.png"];
+    
+    if (self){
+        self.isAlive = NO;
+    }
+
+    return self;
+}
+
+- (void)setIsAlive:(BOOL)newState {
+
+    _isAlive = newState;
+    
+    //inherited property from CCNode class, superclass of CCSprite
+    self.visible = _isAlive;
+}
+
+@end
